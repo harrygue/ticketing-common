59cc31d5b592b1fda2bbe6a282d6f68ab9321f54
harrygue <hg070817.gue@gmail.com>
2021-06-28 20:53:31 +0200

    Updates

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+  yellow: number;
+}
+const color: Color = {
+  red: 20,
+  blue: 30,
+  green: 40,
+  yellow: 20
+};
+
+console.log(color);
+
+export default color;
