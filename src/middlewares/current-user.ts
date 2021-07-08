1a89864228ab89ff5d4b137cbfc2435c7252b1d7
harrygue <hg070817.gue@gmail.com>
2021-07-08 10:05:52 +0200

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,6 +1,5 @@
 import  { Request,Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
-import { isExpressionWithTypeArguments } from 'typescript';
 
 interface UserPayload {
   id: string;
